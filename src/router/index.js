057afea661229852c455e84cd76057d3405b15f1
Leonardo Landin <leolandin123@gmail.com>
2020-07-07 12:43:25 -0300

    SIM

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Home from '@/views/Home.vue'
+
+Vue.use(Router);
+
+const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: Home
+    }
+]
+
+
+const router = new Router({ routes })
+
+
+export default router
