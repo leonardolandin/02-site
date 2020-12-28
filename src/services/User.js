00d9ef6dd36829c2596f6480c4b31345bad60907
Leonardo Landin <leonardo.simoes@reclameaqui.com.br>
2020-12-28 14:53:30 -0300

    e o clone ne

--- src/services/User.js
+++ src/services/User.js
@@ -1,17 +1,11 @@
 import { http } from '@/services/config'
 
-let config = {
-    headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:8080'
-    }
-}
-
 export default {
     getUserLogged: (token) => {
         return http.post(`userLogged`, {token});
     },
     getUserByEmail: (objCredentials) => { 
-        return http.post('login', objCredentials, config);
+        return http.post('login', objCredentials);
     },
     signUser: (userCredentials) => {
         return http.post('register', userCredentials);
